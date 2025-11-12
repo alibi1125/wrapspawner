2ef4c498978b8d20095e12a9f13a20491af4b6b6
Alexander Birk <alexander.birk@ki.uni-stuttgart.de>
2025-11-12 18:19:38 +0100

    Some smaller corrections and improvements.

--- version.py
+++ version.py
@@ -6,7 +6,7 @@ version_info = (
     1,
     1,
     0,
-    'dev9', # comment-out this line for a release
+    'dev10', # comment-out this line for a release
 )
 __version__ = '.'.join(map(str, version_info[:3]))
 
